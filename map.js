bfc4f319c39a6fbf006976f7aec76ccf22bd8b0c
Arthur923 <54887865+Arthur923@users.noreply.github.com>
2020-04-16 19:09:33 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,50 +1,49 @@
-let map1 = new Map();
-let map2 = new Map();
-let map3 = new Map();
+let one = new Map();
+let two = new Map();
+let three = new Map();
 
-// Vurobnuk and name
-map1.set("sklad1", "kola");
-map1.set("sklad2", "fanta");
-map1.set("sklad3", "sprite");
-
-// Price and Vurobnuk
-map2.set(10, "sklad1");
-map2.set(20, "sklad2");
-map2.set(30, "sklad3");
-
-// time and Vurobnuk
-map3.set(1, "sklad1");
-map3.set(2, "sklad2");
-map3.set(3, "sklad3");
-
+// Первое - предполагаемый издатель , 2 - предполагаемые продукт
+one.set("1", "aaa");
+one.set("2", "fff");
+one.set("3", "ccc");
+// Цена
+two.set(11, "1");
+two.set(22, "2");
+two.set(56, "3");
+// Время
+three.set(1, "1");
+three.set(2, "2");
+three.set(3, "3");
+// _______________________________________
+// Готовый definition
 function mapFunc() {
     var price = []
     var time = []
-    for(key of map2.keys()){
-        price.push(key)
-        
+    for(key of two.keys()){
+        price.push(key)        
     }
     var maxPrice = (Math.max(...price))
     var minPrice = (Math.min(...price))
-
-    for(key of map3.keys()){
-        time.push(key)
-        
+    for(key of three.keys()){
+        time.push(key)        
     }
     var maxTime = (Math.max(...time))
     var minTime = (Math.min(...time))
 
-    console.log('Найменша ціна', map1.get(map2.get(minPrice)), minPrice, map2.get(minPrice), getKeyByValue(map3, map2.get(minPrice)))
-
-    console.log('Найбыльша ціна', map1.get(map2.get(maxPrice)), maxPrice, map2.get(maxPrice), getKeyByValue(map3, map2.get(maxPrice)))
-
-    console.log("Найменший час", map1.get(map3.get(minTime)), getKeyByValue(map2, map3.get(minTime)), map3.get(minTime), minTime)
-
-    console.log("Найбільший час", map1.get(map3.get(maxTime)), getKeyByValue(map2, map3.get(maxTime)), map3.get(maxTime), maxTime)
-
+    console.log('Min price', one.get(two.get(minPrice)), minPrice, two.get(minPrice),
+                getKeyByValue(map3, two.get(minPrice)))
+    console.log('Max price', one.get(twp.get(maxPrice)), maxPrice, two.get(maxPrice),
+                getKeyByValue(three, two.get(maxPrice)))
+    console.log("Min time", one.get(three.get(minTime)),
+                getKeyByValue(two, three.get(minTime)), three.get(minTime), minTime)
+    console.log("Max time", map1.get(three.get(maxTime)),
+                getKeyByValue(two, three.get(maxTime)), three.get(maxTime), maxTime)
 }
-
 mapFunc()
+
+
+
+
 
 function getKeyByValue(ob, str){
     for(key of ob.keys()){
@@ -52,10 +51,5 @@ function getKeyByValue(ob, str){
             return key
     }
 }
-
-// for(key of map2.values()){
-    
-//     console.log(key)
-// }
 
 
